b803072a251b17af475a7edae2ee66fc2288c004
Westborn <george@westborn.com.au>
2020-07-05 22:27:09 +1000

    wip: first pass Calendar complete and runs

--- CalendarFunctions.js
+++ CalendarFunctions.js
@@ -1,86 +1,82 @@
 function getCalendarList() {
-  const calendars;
-  let pageToken;
-  const result = [];
+  var calendars
+  let pageToken
+  const result = []
   do {
     calendars = Calendar.CalendarList.list({
       maxResults: 100,
       pageToken: pageToken,
-    });
+    })
     if (calendars.items && calendars.items.length > 0) {
       for (let i = 0; i < calendars.items.length; i++) {
-        const calendar = calendars.items[i];
-        result.push({ summary: calendar.summary, id: calendar.id });
+        const calendar = calendars.items[i]
+        result.push({ summary: calendar.summary, id: calendar.id })
         //        console.log('%s (ID: %s)', calendar.summary, calendar.id)
       }
     } else {
-      console.log("No calendars found.");
+      console.log('No calendars found.')
     }
-    pageToken = calendars.nextPageToken;
-  } while (pageToken);
-  return result;
+    pageToken = calendars.nextPageToken
+  } while (pageToken)
+  return result
 }
 
 // =============================================================================
 // get all the events for the term from the calendar
 
-function downloadCalendarEvents({term,  calendarId} = {}) {
-
+function downloadCalendarEvents({ term, calendarId } = {}) {
   eval(
     UrlFetchApp.fetch(
-      "https://jakubroztocil.github.io/rrule/dist/es5/rrule-tz.min.js"
+      'https://jakubroztocil.github.io/rrule/dist/es5/rrule-tz.min.js'
     ).getContentText()
-  );
+  )
 
   //  console.log('transcribeCalendar')
   //get details of where this app is running from
-  const ss = SpreadsheetApp.getActiveSpreadsheet();
-  const sheetOptions = ss.getSheetByName("Options");
-  const sheetDownload = ss.getSheetByName("Calendar Download");
+  const ss = SpreadsheetApp.getActiveSpreadsheet()
+  const sheetOptions = ss.getSheetByName('Options')
+  const sheetDownload = ss.getSheetByName('Calendar Download')
 
   // setup dates from the term selected (start and end dates)
-  const terms = sheetOptions.getRange(1, 1, 5, 3).getValues();
+  const terms = sheetOptions.getRange(1, 1, 5, 3).getValues()
   const eventRequest = {
-      singleEvents: false,
-      timeMin: new Date(terms[term][1]).toISOString(),
-      timeMax: new Date(terms[term][2]).toISOString(),
-    }
+    singleEvents: false,
+    timeMin: new Date(terms[term][1]).toISOString(),
+    timeMax: new Date(terms[term][2]).toISOString(),
+  }
 
-    //  get the events and format them
-  const courseEvents =  retrieveCalendarEvents(calendarId, eventRequest)
+  //  get the events and format them
+  const courseEvents = retrieveCalendarEvents(calendarId, eventRequest)
   //  console.log(JSON.stringify(events, null, 2))
 
   //clear the sheet we are going to download the events to
-  sheetDownload.insertRowBefore(2);
-  const lastRow = sheetDownload.getLastRow();
+  sheetDownload.insertRowBefore(2)
+  const lastRow = sheetDownload.getLastRow()
   if (lastRow > 2) {
-    sheetDownload.deleteRows(3, lastRow - 2);
+    sheetDownload.deleteRows(3, lastRow - 2)
   }
 
   if (!courseEvents.length) {
-    sheetDownload.getRange(2, 1).setValue("No events Found");
-    return;
+    sheetDownload.getRange(2, 1).setValue('No events Found')
+    return
   }
   //  console.log(courseEvents)
-  const rows = courseEvents.map((d) => flatten_(d));
+  const rows = courseEvents.map((d) => flatten_(d))
   //  console.log(rows)
-  const heads = sheetDownload.getDataRange().offset(0, 0, 1).getValues()[0];
+  const heads = sheetDownload.getDataRange().offset(0, 0, 1).getValues()[0]
 
   // convert object data into a 2d array
-  const tr = rows.map((row) => heads.map((key) => row[String(key)] || ""));
+  const tr = rows.map((row) => heads.map((key) => row[String(key)] || ''))
 
   // write result
-  sheetDownload
-    .getRange(sheetDownload.getLastRow() + 1, 1, tr.length, tr[0].length)
-    .setValues(tr);
+  sheetDownload.getRange(sheetDownload.getLastRow() + 1, 1, tr.length, tr[0].length).setValues(tr)
 
-  return;
+  return
 }
 
 // ====================================================================================
 //Get all necessary course events (dates, location, summary, description) as an array of objects
 function retrieveCalendarEvents(calendarId, eventRequest) {
-
   const unpackEvent = (type, event) => {
     const courseEvent = {
       type: type,
@@ -107,9 +103,10 @@ function retrieveCalendarEvents(calendarId, eventRequest) {
 
     if (event.recurrence) {
       courseEvent.recurrence = event.recurrence[0]
-      const tmp = rrule.RRule.fromString(event.recurrence[0])
+      const dtStart = dateStringUTC(event.start.dateTime)
+      const tmp = rrule.RRule.fromString(`DTSTART:${dtStart}\n${event.recurrence[0]}`)
       courseEvent.recurText = decodeRecurText(tmp)
-      courseEvent.recurDates = decodeRecurDates(tmp, '2020-06-01T12:00:00-10:00')
+      courseEvent.recurDates = decodeRecurDates(tmp)
     }
 
     const entryPoints = getNested(event, 'conferenceData', 'entryPoints')
@@ -180,9 +177,9 @@ function retrieveCalendarEvents(calendarId, eventRequest) {
       }
     })
   })
-  console.log(courseEvents.length)
+  // console.log(courseEvents.length)
   courseEvents.sort(sortByTypeAndDate)
-  courseEvents.forEach((e) => console.log(`${e.type} - ${e.id} - ${e.summary}`))
+  // courseEvents.forEach((e) => console.log(`${e.type} - ${e.id} - ${e.summary}`))
   //  console.log("\n\nException")
   //  exception.forEach(e => console.log(`${e.type} - ${e.id} - ${e.summary}`))
   //  console.log("\n\nRecurrent")
@@ -191,44 +188,65 @@ function retrieveCalendarEvents(calendarId, eventRequest) {
   return courseEvents
 }
 
-
 function getNested(obj, ...args) {
   return args.reduce((obj, level) => obj && obj[level], obj)
 }
 const monthNames = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+]
 
-const splitDate = (t = new Date()) => t.toLocaleString().split(/[^\d]/);
+const splitDate = (t = new Date()) => t.toLocaleString().split(/[^\d]/)
 
 const fmtDate = (dtStr) => {
-  if (typeof dtStr === "undefined" || dtStr === "") return "unknown";
-  const [d, m, y] = splitDate(new Date(dtStr));
-  return d + "-" + monthNames[m - 1] + "-" + y;
-};
+  if (typeof dtStr === 'undefined' || dtStr === '') return 'unknown'
+  const [d, m, y] = splitDate(new Date(dtStr))
+  return d + '-' + monthNames[m - 1] + '-' + y
+}
 
-const decodeRecurText = (rule) => rule.toText();
+const decodeRecurText = (rule) => rule.toText()
 
-const decodeRecurDates = (eventRule, dte = new Date()) => {
+const decodeRecurDates = (eventRule) => {
   // console.log(`decodeDates: ${JSON.stringify(eventRule, null, 2)}`)
-  const newRule = new rrule.RRule({
-    ...eventRule.origOptions,
-    dtstart: new Date(dte),
-  });
   // just return dd-mmm (max 5)
-  const futureDates = newRule
+  const futureDates = eventRule
     .all((date, i) => i < 6)
-    .map((dte) => fmtDate(new Date(dte)).slice(0, 6));
-  return `${futureDates.join(", ")}${futureDates.length > 5 ? "..." : ""}`;
-};
+    .map((dte) => fmtDate(new Date(dte)).slice(0, 6))
+  return `${futureDates.join(', ')}${futureDates.length > 5 ? '...' : ''}`
+}
+// Based on https://stackoverflow.com/a/54897035/1027723
+const flatten_ = (obj, prefix = '', res = {}) =>
+  Object.entries(obj).reduce((r, [key, val]) => {
+    const k = `${prefix}${key}`
+    if (typeof val === 'object' && val !== null) {
+      flatten_(val, `${k}_`, r)
+    } else {
+      res[k] = val
+    }
+    return r
+  }, res)
+
+const dateStringUTC = (dateTime) => {
+  const date = new Date(dateTime)
+  const mm = date.getUTCMonth() + 1
+  return [
+    date.getUTCFullYear().toString().padStart(4, '0'),
+    mm.toString().padStart(2, '0'),
+    date.getUTCDate().toString().padStart(2, '0'),
+    'T',
+    date.getUTCHours().toString().padStart(2, '0'),
+    date.getUTCMinutes().toString().padStart(2, '0'),
+    date.getUTCSeconds().toString().padStart(2, '0'),
+    'Z',
+  ].join('')
+}
